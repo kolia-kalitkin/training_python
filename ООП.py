dd9decd635af49f2a71d245dc9258a8e536ec71d
kolia-kalitkin <nikolay.kalitkin@gmail.com>
2023-12-16 19:57:17 +0600

    16.12.23

--- ООП.py
+++ ООП.py
@@ -877,23 +877,142 @@ class Person:
 # ---------------------------------------------------------------
 
 
+
+#  Person
 #
-#
-#
+# Вам доступен класс Person, описывающий человека. При создании экземпляра класс принимает два аргумента в следующем порядке:
+#     name — имя человека
+#     surname — фамилия человека
+# Экземпляр класса Person имеет два атрибута:
+#     name — имя человека
+#     surname — фамилия человека
+# Класс Person имеет одно свойство:
+#     fullname — свойство, доступное для чтения и записи, возвращающее полное имя человека в виде строки:
+#     <имя> <фамилия>
+# Реализуйте свойство fullname класса Person с помощью декоратора @property.
+# Примечание 1. При изменении имени и/или фамилии человека должно изменяться и его полное имя. Аналогично при изменении полного имени должны изменяться имя и фамилия.
+# ---------------------------------------------------------------
+class Person:
+    def __init__(self, name, surname) -> None:
+        self._name = name
+        self._surname = surname
+
+    @property
+    def fullname(self):
+        return "{} {}".format(self.name, self.surname)   
+
+    @fullname.setter
+    def fullname(self, fullname):
+        self.name, self.surname = fullname.split()
+        
+# БЕЗ ДЕКОРАТОРА
+
+class Person:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def get_fullname(self):
+        return self.name + ' ' + self.surname
+
+    def set_fullname(self, fullname):
+        self.name, self.surname = fullname.split()
+
+    fullname = property(get_fullname, set_fullname)
+
+
 # ---------------------------------------------------------------
 
 # ---------------------------------------------------------------
 
+
+# Класс Account
+# 956
+# В целях безопасности в базах данных пароли от аккаунтов пользователей хранятся не в явном виде, а в виде хеш-значений — чисел, вычисленных по специальному алгоритму на основе паролей.
+# Вам доступна функция hash_function(), которая принимает в качестве аргумента пароль и возвращает его хеш-значение.
+# Реализуйте класс Account, описывающий аккаунт интернет-пользователя на некотором сервисе. При создании экземпляра класс должен принимать два аргумента в следующем порядке:
+#     login — логин пользователя
+#     password — пароль пользователя
+# Класс Account должен иметь два свойства:
+#     login — свойство, доступное только для чтения, возвращающее логин пользователя. При попытке изменения свойства должно быть возбуждено исключение AttributeError с текстом:
+#     Изменение логина невозможно
+#     password — свойство, доступное для чтения и записи, возвращающее хеш-значение пароля от аккаунта пользователя. При изменении свойство должно вычислять хеш-значение нового пароля и сохранять его, а не сам пароль
+# Примечание 1. Дополнительная проверка данных на корректность не требуется. Гарантируется, что реализованный класс используется только с корректными данными.
+# Примечание 2. Никаких ограничений касательно реализации класса Account нет, она может быть произвольной.
 # ---------------------------------------------------------------
 
 
-#
-#
-#
-# ---------------------------------------------------------------
+class Account:
+    def __init__(self, login, password) -> None:
+        self._login = login
+        self._password = hash_function(password)    
+    
 
-# ---------------------------------------------------------------
+    # ---------------------------------------------------------------
+    @property
+    def login(self):        # свойство доступно только для чтения (сеттер выдаст ошибку при попытке изменить логин)
+        return self._login
 
+    @login.setter
+    def login(self, login):       
+        raise AttributeError('Изменение логина невозможно')
+    # ---------------------------------------------------------------
+
+    @property
+    def password(self):        # свойство доступно для чтения и записи
+        return self._password
+
+    @password.setter
+    def password(self, password):        
+        self._password = hash_function(password)
+        
+        
+def hash_function(password):
+    '''ф-я принимает в качестве аргумента пароль и возвращает его хеш-значение.'''
+    hash_value = 0
+    for char, index in zip(password, range(len(password))):
+        hash_value += ord(char) * index
+    return hash_value % 10**9
+
+
+
+
+
+
+account = Account('timyr-guev', 'lovebeegeek')
+
+print(account.password)
+account.password = 'verylovebeegeek'
+print(account.password)
+
+# ------------------препод-------------------------------------
+def hash_function(password):
+    hash_value = 0
+    for char, index in zip(password, range(len(password))):
+        hash_value += ord(char) * index
+    return hash_value % 10 ** 9
+
+
+class Account:
+    def __init__(self, login, password):
+        self._login = login
+        self.password = password
+
+    @property
+    def login(self):
+        return self._login
+
+    @login.setter
+    def login(self, login):
+        raise AttributeError('Изменение логина невозможно')
+            
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        self._password = hash_function(password)
 # ---------------------------------------------------------------
 
 
